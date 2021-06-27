78d1e5919097548e14b6afbefd26f1044e415a1c
farhansutrisno <farhansutrisno98@gmail.com>
2021-06-27 18:28:56 +0700

    update

---
+++ application/views/webbackend/V_laporanHistoryReservasi.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html><head>
+	<title></title>
+</head><body>
+
+	<?php if ($filter['filter'] == 1) { ?>
+		  
+		  <table border="0">
+
+		    
+		    <tr>
+		        <td>Filter</td>
+		        <td></td>
+		    </tr>
+		    
+		    <?php 
+		    if (!empty($filter['status'])) { ?>
+		      <tr>
+		          <td>status :</td>
+		          <td><?php echo $filter['status']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <?php 
+		    if (!empty($filter['bulan'])) { ?>
+		      <tr>
+		          <td>Bulan :</td>
+		          <td><?php echo $filter['bulan']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <?php 
+		    if (!empty($filter['tahun'])) { ?>
+		      <tr>
+		          <td>Tahun :</td>
+		          <td><?php echo $filter['tahun']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <tr>
+		        <td></td>
+		        <td></td>
+		    </tr>
+		</table>
+
+	<?php } ?>
+
+	<table border="0" class="table">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Nama Konsumen</th>
+				<th>No Telepon</th>
+				<th>No Plat</th>
+				<th>No Antrian</th>
+				<th>Total Bayar</th>
+				<th>Status Pembelian</th>
+				<th>Tgl Transaksi</th>
+				<th>Kode Reservasi</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+	            $no=1;
+                foreach ($penjualan as $key){
+
+                    if (empty($key->tglTransaksi)) {
+                      $dateUpdate = '-';
+                    }else{
+                      $dateUpdate = date_format (new DateTime($key->tglTransaksi), 'd M Y').' '.date_format (new DateTime($key->tglPembayaran), 'H:i');
+                    }
+
+                    if (empty($key->KdTukang)) {
+                      $KdTukang = 0;
+                    }else{
+                      $KdTukang = $key->KdTukang;
+                    }
+
+                    echo "<tr>";
+	                    echo "<td>". $no++."</td>";
+	                    echo "<td>". $key->namaLengkap."</td>";
+	                    echo "<td>". $key->noTelepon."</td>";
+	                    echo "<td>". $key->noPlat."</td>";
+	                    echo "<td>". $key->noAntrian."</td>";
+	                    echo "<td>Rp.".number_format($key->totalBayar, 0,",",".")."</td>";
+	                    echo "<td style='color: #5CB85C !important;'><b>". $key->statusPembayaran."</b></td>";
+	                    echo "<td>". $dateUpdate."</td>";
+	                    echo "<td>". $key->kodeUnik."</td>";
+	          ?>
+	                	</tr>
+	            <?php } ?>
+	    </tbody>
+	</table>
+</body></html>
---
+++ application/views/webbackend/V_laporanProduk.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html><head>
+	<title></title>
+</head><body>
+
+	<?php if ($filter['filter'] == 1) { ?>
+		  
+		  <table border="0">
+
+		    
+		    <tr>
+		        <td>Filter</td>
+		        <td></td>
+		    </tr>
+		    
+		    <?php 
+		    if (!empty($filter['kategori'])) { ?>
+		      <tr>
+		          <td>Kategori :</td>
+		          <td><?php echo $filter['kategori']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <?php 
+		    if (!empty($filter['bulan'])) { ?>
+		      <tr>
+		          <td>Bulan :</td>
+		          <td><?php echo $filter['bulan']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <?php 
+		    if (!empty($filter['tahun'])) { ?>
+		      <tr>
+		          <td>Tahun :</td>
+		          <td><?php echo $filter['tahun']; ?></td>
+		      </tr>
+		    <?php } ?>
+
+		    <tr>
+		        <td></td>
+		        <td></td>
+		    </tr>
+		</table>
+
+	<?php } ?>
+
+	<table border="0" class="table">
+		<thead>
+			<tr>
+				<th style="width: 10px !important;">No</th>
+				<th>Nama Layanan</th>
+				<th>Kategori</th>
+				<th>Paket</th>
+				<th>Harga</th>
+				<th style="text-align: center !important;">Create date</th>
+				<th style="text-align: center !important;">Update date</th>
+			</tr>
+		</thead>
+		<tbody>
+          <?php
+            $no=1;
+                foreach ($produk as $key){
+
+                  if (empty($key->updateDate)) {
+                    $dateUpdate = '-';
+                  }else{
+                    $dateUpdate = date_format (new DateTime($key->updateDate), 'd-M-Y');
+                  }
+
+                  if (empty($key->createDate)) {
+                    $dateCreate = '-';
+                  }else{
+                    $dateCreate = date_format (new DateTime($key->createDate), 'd-M-Y');
+                  }
+
+                echo "<tr>";
+                    echo "<td>". $no++."</td>";
+                    echo "<td>". $key->namaProduk."</td>";
+                    echo "<td>". $key->kategori."</td>";
+                    echo "<td>". $key->paket."</td>";
+                    echo "<td>Rp.".number_format($key->hargaPenjualan, 0,",",".")."</td>";
+                    echo "<td align='center'>". $dateCreate."</td>";
+                    echo "<td align='center'>". $dateUpdate."</td>";
+                    ?>
+                    
+                </tr>
+            <?php } ?>
+          
+        </tbody>
+	</table>
+</body></html>
